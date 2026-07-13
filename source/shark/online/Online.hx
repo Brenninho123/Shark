ad9055eca5af7baa58bd921c807b5fb45f2b175e
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-07-13 16:35:04 -0300

    Refactor online status checking with multiple URLs

--- source/shark/online/Online.hx
+++ source/shark/online/Online.hx
@@ -8,33 +8,57 @@ class Online
 	public static var isOnline(default, null):Bool = false;
 	public static var onStatusChanged:Bool->Void;
 
-	public static var checkUrl:String = "https://www.google.com";
-	public static var checkInterval:Float = 15;
+	public static var checkUrls:Array<String> = [
+		"https://www.google.com",
+		"https://www.cloudflare.com",
+		"https://www.apple.com"
+	];
+
+	public static var onlineCheckInterval:Float = 20;
+	public static var offlineCheckInterval:Float = 5;
+	public static var checkTimeoutMs:Int = 8000;
+
+	public static var consecutiveFailures(default, null):Int = 0;
+	public static var lastCheckTime(default, null):Float = 0;
 
 	static var timer:Timer;
 	static var isChecking:Bool = false;
+	static var isRunning:Bool = false;
 
 	public static function start():Void
 	{
+		isRunning = true;
 		checkNow();
-
-		if (timer != null)
-			timer.stop();
-
-		timer = new Timer(Std.int(checkInterval * 1000));
-		timer.run = function():Void
-		{
-			checkNow();
-		};
+		scheduleNext();
 	}
 
 	public static function stop():Void
 	{
+		isRunning = false;
+
 		if (timer != null)
 		{
 			timer.stop();
 			timer = null;
 		}
+	}
+
+	static function scheduleNext():Void
+	{
+		if (!isRunning)
+			return;
+
+		if (timer != null)
+			timer.stop();
+
+		var interval:Float = isOnline ? onlineCheckInterval : offlineCheckInterval;
+
+		timer = new Timer(Std.int(interval * 1000));
+		timer.run = function():Void
+		{
+			checkNow();
+			scheduleNext();
+		};
 	}
 
 	public static function checkNow(?onResult:Bool->Void):Void
@@ -43,12 +67,47 @@ class Online
 			return;
 
 		isChecking = true;
+		lastCheckTime = Timer.stamp();
 
-		var http = new Http(checkUrl);
+		attemptCheck(0, onResult);
+	}
+
+	static function attemptCheck(urlIndex:Int, ?onResult:Bool->Void):Void
+	{
+		if (urlIndex >= checkUrls.length)
+		{
+			isChecking = false;
+			consecutiveFailures++;
+			setStatus(false);
+
+			if (onResult != null)
+				onResult(false);
+
+			return;
+		}
+
+		var http = new Http(checkUrls[urlIndex]);
+		var finished:Bool = false;
+
+		var timeoutTimer = Timer.delay(function():Void
+		{
+			if (finished)
+				return;
+
+			finished = true;
+			attemptCheck(urlIndex + 1, onResult);
+		}, checkTimeoutMs);
 
 		http.onData = function(data:String):Void
 		{
+			if (finished)
+				return;
+
+			finished = true;
+			timeoutTimer.stop();
+
 			isChecking = false;
+			consecutiveFailures = 0;
 			setStatus(true);
 
 			if (onResult != null)
@@ -57,11 +116,13 @@ class Online
 
 		http.onError = function(msg:String):Void
 		{
-			isChecking = false;
-			setStatus(false);
+			if (finished)
+				return;
 
-			if (onResult != null)
-				onResult(false);
+			finished = true;
+			timeoutTimer.stop();
+
+			attemptCheck(urlIndex + 1, onResult);
 		};
 
 		http.request(false);
@@ -69,12 +130,16 @@ class Online
 
 	static function setStatus(value:Bool):Void
 	{
-		if (isOnline == value)
-			return;
-
+		var wasOnline:Bool = isOnline;
 		isOnline = value;
 
-		if (onStatusChanged != null)
-			onStatusChanged(value);
+		if (wasOnline != value)
+		{
+			if (onStatusChanged != null)
+				onStatusChanged(value);
+
+			if (isRunning)
+				scheduleNext();
+		}
 	}
 }
