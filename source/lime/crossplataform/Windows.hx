c35ea1899bc306e52bffcef6e61feef00f8f957a
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-07-14 07:03:49 -0300

    Add Windows configuration class for HXProject

---
+++ source/lime/crossplataform/Windows.hx
@@ -0,0 +1,49 @@
+package lime.crossplataform;
+
+import lime.tools.HXProject;
+import lime.tools.Icon;
+
+class Windows
+{
+	public static function configure(project:HXProject):Void
+	{
+		configureWindow(project);
+		configureDefines(project);
+		configureOptimizations(project);
+		configureIcon(project);
+	}
+
+	static function configureWindow(project:HXProject):Void
+	{
+		project.window.width = 1280;
+		project.window.height = 720;
+		project.window.fps = 60;
+		project.window.background = 0x00111F;
+		project.window.hardware = true;
+		project.window.vsync = false;
+		project.window.antialiasing = 0;
+		project.window.resizable = true;
+	}
+
+	static function configureDefines(project:HXProject):Void
+	{
+		project.haxedefs.set("SHARK_PLATFORM", "windows");
+
+		if (!project.debug)
+			project.haxedefs.set("FLX_NO_FOCUS_LOST_SCREEN", "");
+	}
+
+	static function configureOptimizations(project:HXProject):Void
+	{
+		if (project.debug)
+			return;
+
+		project.haxeflags.push("-dce full");
+		project.haxeflags.push("--no-traces");
+	}
+
+	static function configureIcon(project:HXProject):Void
+	{
+		project.icons.push(new Icon("assets/images/icon.png"));
+	}
+}
