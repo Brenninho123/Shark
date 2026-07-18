6603e61c2c9dae938bf3d5de7222ca1179e0e1ac
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-07-18 20:40:24 -0300

    Refactor Discord class to remove CPP dependencies
    Removed CPP-specific Discord RPC functionality and added empty implementations for related functions.

--- source/shark/ui/discord/Discord.hx
+++ source/shark/ui/discord/Discord.hx
@@ -1,87 +1,18 @@
 package shark.ui.discord;
 
-#if cpp
-import discord_rpc.DiscordRpc;
-#end
-
 class Discord
 {
-	public static var isSupported(default, null):Bool;
+	public static var isSupported(default, null):Bool = false;
 	public static var isConnected(default, null):Bool = false;
-
-	static var applicationId:String = "";
-	static var startTimestamp:Float = 0;
 
 	public static function initialize(clientId:String):Void
 	{
-		#if cpp
-		isSupported = true;
-		applicationId = clientId;
-		startTimestamp = Date.now().getTime() / 1000;
-
-		DiscordRpc.start({
-			clientID: clientId,
-			onReady: onReady,
-			onDisconnected: onDisconnected,
-			onError: onError
-		});
-		#else
 		isSupported = false;
-		#end
 	}
 
-	static function onReady():Void
-	{
-		isConnected = true;
-	}
+	public static function update(state:String, details:String, ?largeImageKey:String, ?largeImageText:String, resetTimestamp:Bool = false):Void {}
 
-	static function onDisconnected(errorCode:Int, message:String):Void
-	{
-		isConnected = false;
-	}
+	public static function runCallbacks():Void {}
 
-	static function onError(errorCode:Int, message:String):Void
-	{
-		isConnected = false;
-	}
-
-	public static function update(state:String, details:String, ?largeImageKey:String, ?largeImageText:String, resetTimestamp:Bool = false):Void
-	{
-		#if cpp
-		if (!isSupported)
-			return;
-
-		if (resetTimestamp)
-			startTimestamp = Date.now().getTime() / 1000;
-
-		DiscordRpc.presence({
-			state: state,
-			details: details,
-			startTimestamp: Std.int(startTimestamp),
-			largeImageKey: largeImageKey,
-			largeImageText: largeImageText
-		});
-		#end
-	}
-
-	public static function runCallbacks():Void
-	{
-		#if cpp
-		if (!isSupported)
-			return;
-
-		DiscordRpc.process();
-		#end
-	}
-
-	public static function shutdown():Void
-	{
-		#if cpp
-		if (!isSupported)
-			return;
-
-		DiscordRpc.shutdown();
-		isConnected = false;
-		#end
-	}
+	public static function shutdown():Void {}
 }
