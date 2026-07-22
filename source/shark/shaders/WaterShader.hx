0b099171952cdfe7183158f5f4551acc1391a84f
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-07-22 09:22:42 -0300

    Update texture sampling method in WaterShader

--- source/shark/shaders/WaterShader.hx
+++ source/shark/shaders/WaterShader.hx
@@ -1,6 +1,6 @@
 package shark.shaders;
 
-import flixel.system.FlxShader;
+import flixel.system.FlxAssets.FlxShader;
 
 class WaterShader extends FlxShader
 {
@@ -20,7 +20,7 @@ class WaterShader extends FlxShader
 			uv.x += sin(uv.y * uFrequency + uTime) * uAmplitude;
 			uv.y += cos(uv.x * uFrequency + uTime * 0.8) * uAmplitude;
 
-			vec4 color = texture2D(bitmap, uv);
+			vec4 color = flixel_texture2D(bitmap, uv);
 			color.rgb = mix(color.rgb, uTint, uTintStrength * color.a);
 
 			gl_FragColor = color * openfl_Alpha;
