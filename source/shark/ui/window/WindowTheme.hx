82319728d38638c88194d1e313a5d656eb3dbc86
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-17 18:03:43 -0300

    Refactor WindowTheme class and remove Windows dependency

--- source/shark/ui/window/WindowTheme.hx
+++ source/shark/ui/window/WindowTheme.hx
@@ -1,94 +1,24 @@
 package shark.ui.window;
 
-#if windows
-import hxwindowmode.WindowColorMode;
-#end
-
 class WindowTheme
 {
-	public static var isSupported(default, null):Bool;
+	public static var isSupported(default, null):Bool = false;
 	public static var isDarkMode(default, null):Bool = true;
 
-	static var HEADER_COLOR:Array<Int> = [1, 17, 31];
-	static var BORDER_COLOR:Array<Int> = [97, 165, 194];
-	static var TITLE_COLOR:Array<Int> = [224, 251, 252];
+	public static function initialize():Void {}
 
-	public static function initialize():Void
-	{
-		#if windows
-		isSupported = true;
-		applyAquaticTheme();
-		#else
-		isSupported = false;
-		#end
-	}
+	public static function applyAquaticTheme():Void {}
 
-	public static function applyAquaticTheme():Void
-	{
-		#if windows
-		try
-		{
-			WindowColorMode.setDarkMode();
-			WindowColorMode.setWindowBorderColor(HEADER_COLOR, true, true);
-			WindowColorMode.setWindowTitleColor(TITLE_COLOR);
-			WindowColorMode.redrawWindowHeader();
+	public static function setDark():Void {}
 
-			isDarkMode = true;
-		}
-		catch (e:Dynamic) {}
-		#end
-	}
-
-	public static function setDark():Void
-	{
-		#if windows
-		try
-		{
-			WindowColorMode.setDarkMode();
-			WindowColorMode.redrawWindowHeader();
-			isDarkMode = true;
-		}
-		catch (e:Dynamic) {}
-		#end
-	}
-
-	public static function setLight():Void
-	{
-		#if windows
-		try
-		{
-			WindowColorMode.setLightMode();
-			WindowColorMode.redrawWindowHeader();
-			isDarkMode = false;
-		}
-		catch (e:Dynamic) {}
-		#end
-	}
+	public static function setLight():Void {}
 
 	public static function toggle():Bool
 	{
-		if (isDarkMode)
-			setLight();
-		else
-			setDark();
-
 		return isDarkMode;
 	}
 
-	public static function setBorderColor(r:Int, g:Int, b:Int, header:Bool = true, border:Bool = true):Void
-	{
-		#if windows
-		try
-		{
-			WindowColorMode.setWindowBorderColor([r, g, b], header, border);
-			WindowColorMode.redrawWindowHeader();
-		}
-		catch (e:Dynamic) {}
-		#end
-	}
+	public static function setBorderColor(r:Int, g:Int, b:Int, header:Bool = true, border:Bool = true):Void {}
 
-	public static function resetToAquaticTheme():Void
-	{
-		applyAquaticTheme();
-	}
+	public static function resetToAquaticTheme():Void {}
 }
